17e1a8c9784d54af7f8390739892a86009bfbed2
Wade Novak <wade@launchpadlab.com>
2018-10-08 08:45:15 -0500

    Adds subscriber resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :accommodations
-    resources :suggestions
+    resources :attractions
     resources :events
     resources :images
     resources :neighborhoods
     resources :rsvps
     resources :rsvp_invites
+    resources :subscribers
+    resources :suggestions
     resources :tags
-    resources :attractions
 
     root to: "accommodations#index"
   end
 
-  resources :rsvps
-  resources :attractions
   resources :accommodations
+  resources :attractions
   resources :events
+  resources :rsvps
+  resources :subscribers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
